703668867be1e248d878e809c4e2f3bdeb16e96f
michallangier <m.langier@wp.pl>
2022-05-28 15:43:34 +0200

    poprawione

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const isOdd = require("is-odd");
-let rok = 2004;
-let nieJestPrzestepny = `Rok ${rok} nie jest przestępny`;
-let jestPrzestepny = `Rok ${rok} jest przestępny`;
-let wynik = isOdd(rok) ? nieJestPrzestepny : isOdd(rok / 2) ? nieJestPrzestepny : jestPrzestepny;
-console.log(wynik);
+let year = -8;
+
+try {
+    if (Number.isInteger(year)) console.log(`${year} - correct value (integer).`)
+    else {
+        console.log(`${year} is not an integer, type in correct value.`)
+        process.exit(1)
+    }
+} catch {
+    throw console.log("Unexpected error.")
+}
+
+
+if ((year % 4) === 0) console.log(`Year: ${year} is a leap year`)
+else console.log(`Year: ${year} is not a leap year`)
